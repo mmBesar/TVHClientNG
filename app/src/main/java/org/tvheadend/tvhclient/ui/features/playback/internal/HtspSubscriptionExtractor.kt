54801b2ad11adcaa64e079d58d878bada3fe1a2c
Mohammed Besar <35855690+mmBesar@users.noreply.github.com>
2026-04-03 02:46:21 +0200

    Fix SeekPoint constructor for Media3 1.2.1 compatibility

--- app/src/main/java/org/tvheadend/tvhclient/ui/features/playback/internal/HtspSubscriptionExtractor.kt
+++ app/src/main/java/org/tvheadend/tvhclient/ui/features/playback/internal/HtspSubscriptionExtractor.kt
@@ -42,7 +42,7 @@ internal class HtspSubscriptionExtractor : Extractor {
     private class HtspSeekMap : SeekMap {
         override fun isSeekable(): Boolean = true
         override fun getDurationUs(): Long = C.TIME_UNSET
-        override fun getSeekPoints(timeUs: Long): SeekMap.SeekPoints = SeekMap.SeekPoints(SeekMap.SeekPoint.START)
+        override fun getSeekPoints(timeUs: Long): SeekMap.SeekPoints = SeekMap.SeekPoints(SeekMap.SeekPoint(0, 0))
     }
 
     @Throws(IOException::class)
